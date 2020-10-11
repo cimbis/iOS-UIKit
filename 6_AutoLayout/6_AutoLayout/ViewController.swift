9271df9df95eadb4d013547a8deaf5f2d83dc8a6
JC <cimbulis.j@gmail.com>
2020-10-11 17:19:09 +0300

    little bit of auto layout

---
+++ 6_AutoLayout/6_AutoLayout/ViewController.swift
@@ -0,0 +1,116 @@
+//
+//  ViewController.swift
+//  6_AutoLayout
+//
+//  Created by Janis Cimbulis on 11/10/2020.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        createLabels()
+    }
+
+    func createLabels() {
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = UIColor.red
+        label1.text = "THESE"
+        label1.sizeToFit()
+
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = UIColor.cyan
+        label2.text = "ARE"
+        label2.sizeToFit()
+
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = UIColor.yellow
+        label3.text = "SOME"
+        label3.sizeToFit()
+
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = UIColor.green
+        label4.text = "AWESOME"
+        label4.sizeToFit()
+
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = UIColor.orange
+        label5.text = "LABELS"
+        label5.sizeToFit()
+
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+
+        //        let viewsDictionary = [
+        //            "label1": label1,
+        //            "label2": label2,
+        //            "label3": label3,
+        //            "label4": label4,
+        //            "label5": label5
+        //        ]
+        //
+        //        let metrics = [
+        //            "labelHeight": 88
+        //        ]
+        //
+        //        for label in viewsDictionary.keys {
+        //            view.addConstraints(NSLayoutConstraint.constraints(
+        //                    withVisualFormat: "H:|[\( label )]|",
+        //                    options: [],
+        //                    metrics: nil,
+        //                    views: viewsDictionary
+        //            ))
+        //        }
+        //
+        //        view.addConstraints(NSLayoutConstraint.constraints(
+        //                withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|",
+        //                options: [],
+        //                metrics: metrics,
+        //                views: viewsDictionary
+        //        ))
+
+        let labelArray = [label1, label2, label3, label4, label5]
+
+        for (index, label) in labelArray.enumerated() {
+            label.widthAnchor
+                    .constraint(equalTo: view.widthAnchor)
+                    .isActive = true
+
+            label.heightAnchor
+                    .constraint(equalToConstant: 88)
+                    .isActive = true
+
+            if index > 0 {
+                // 2nd label and onwards
+                label.topAnchor
+                        .constraint(equalTo: labelArray[index - 1].bottomAnchor, constant: 10)
+                        .isActive = true
+            }
+            else if index == labelArray.count {
+                // last label
+                label.bottomAnchor
+                        .constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+                        .isActive = true
+            }
+            else {
+                // 1st label
+                label.topAnchor
+                        .constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0)
+                        .isActive = true
+            }
+        }
+    }
+}
+
